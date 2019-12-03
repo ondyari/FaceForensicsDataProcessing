6274ce8e8779d52219c67589554c609894421bdc
Christoph Clement <christoph.clement@tum.de>
2019-12-03 12:24:30 +0100

    Include masks in migration

--- src/faceforensics_internal/scripts/migrate_bounding_boxes_to_face_information.py
+++ src/faceforensics_internal/scripts/migrate_bounding_boxes_to_face_information.py
@@ -19,14 +19,24 @@ def migrate_bounding_boxes_to_face_information(source_dir_root, methods):
     source_dir_data_structure = FaceForensicsDataStructure(
         source_dir_root,
         methods=methods,
-        compressions=(Compression.c40, Compression.c23, Compression.raw),
+        compressions=(
+            Compression.c40,
+            Compression.c23,
+            Compression.raw,
+            Compression.masks,
+        ),
         data_types=(DataType.bounding_boxes,),
     )
 
     target_dir_data_structure = FaceForensicsDataStructure(
         source_dir_root,
         methods=methods,
-        compressions=(Compression.c40, Compression.c23, Compression.raw),
+        compressions=(
+            Compression.c40,
+            Compression.c23,
+            Compression.raw,
+            Compression.masks,
+        ),
         data_types=(DataType.face_information,),
     )
 
